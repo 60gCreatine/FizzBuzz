10dff80cfe55b3777f4f9764d48878f5bb66c731
60gCreatine <cphah643@gmail.com>
2025-01-06 23:04:40 +0100

    FizzBuzz in Python

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+for i in range(1, 101):
+    sleep(0.2)
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else: print(i)
